c3fc5e58f2acc1d267d8f17864192775821aeda1
sctavo <gsgsnchz09@gmail.com>
2025-11-17 01:27:18 -0300

    Etapa 3 - Script y cambios en .YML

--- README.md
+++ README.md
@@ -6,6 +6,8 @@ docker-compose up -d
 
 en esa misma terminal, cuando ya se creen los contenedores:
 
+docker exec -it spark-client pip install numpy
+
 docker exec -it spark-client /opt/spark/bin/pyspark
 
 Esto generará una terminal de spark en la que se podrán probar los scripts que están en la carpeta notebooks 
---
+++ scripts/etl_vuelos.py
@@ -0,0 +1,32 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, when
+
+spark = SparkSession.builder.appName("ETL_Vuelos").getOrCreate()
+
+# Leer CSV
+df = spark.read.csv("/opt/spark/data/flight_data_2024.csv", header=True, inferSchema=True)
+
+# Renombrar columnas para trabajar uniforme
+df = df.select(
+    col("fl_date").alias("FlightDate"),
+    col("op_unique_carrier").alias("Airline"),
+    col("origin").alias("Origin"),
+    col("dest").alias("Dest"),
+    col("dep_delay").alias("DepDelay"),
+    col("arr_delay").alias("ArrDelay"),
+    col("distance").alias("Distance")
+)
+
+# Limpiar valores nulos
+df = df.dropna(subset=["ArrDelay", "DepDelay"])
+
+# Variable objetivo: retraso mayor a 15 minutos
+df = df.withColumn("Retrasado", when(col("ArrDelay") > 15, 1).otherwise(0))
+
+# Guardar resultado
+df.write.mode("overwrite").parquet("/opt/spark/work-dir/output/cleaned_flights")
+
+
+
+print("ETL COMPLETADO EXITOSAMENTE")
+spark.stop()
